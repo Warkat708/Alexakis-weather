c83cb7e945da719c6c376365cb8cdd719bf0995b
Kat Alexakis <katherine.alexakis@gmail.com>
2021-04-09 15:11:23 +0100

    Icon now updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 let now = new Date ();
 let day = now.getDay ();
 let number = now.getDate ();
@@ -34,20 +36,36 @@ let season = months[now.getMonth()];
 let minAdjust = (`0${min}`); 
 
 
-
-console.log(season);
-
 let beautiful = `${today} ${hour}:${min}`
-console.log (beautiful);
 
 date = document.querySelector("#currentTime")
 date.innerHTML = beautiful;
 
+function displayWeather(response) {
+  console.log (response.data);
+  let descriptionElement = document.querySelector("#description");
+  let cityElement = document.querySelector("#city");
+  let degreesElement = document.querySelector("#degrees");
+  let realFeel = document.querySelector("#realFeel");
+  let windElement = document.querySelector("#wind"); 
+  let iconElement = document.querySelector("#icon");
+  let humidityElement = document.querySelector("#humidity");
+
+descriptionElement.innerHTML = response.data.weather[0].main; 
+windElement.innerHTML = Math.round(response.data.wind.speed);
+degreesElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+realFeel.innerHTML = Math.round(response.data.main.feels_like); 
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+humidityElement.innerHTML = response.data.main.humidity;
+}
+
 function foreground (event) {
   event.preventDefault (); 
-  let target = document.querySelector ("h1");
-let input = document.querySelector ("#citySearch");
-  target.innerHTML = input.value;  
+  let apiKey = "5fe458900dd572eaefce331c17176dd2";
+  let place = document.querySelector("#citySearch").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
 let form = document.querySelector("form");
@@ -61,11 +79,42 @@ data.innerHTML = "60";
 let toggle = document.querySelector ("#farenheit-link");
 toggle.addEventListener ("click", faren);
 
+function showTemp(response) {
+  let temperature = response.data.main.temp; 
+  console.log(temperature); 
+}
+
+let city = "Bristol";
+let apiKey = "5fe458900dd572eaefce331c17176dd2";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+
+axios.get(apiUrl).then(showTemp);
+
 function celsius (event) {
   event.preventDefault (); 
 let data = document.querySelector("#degrees"); 
-data.innerHTML = "19";
+data.innerHTML = response.data.main.temp;
 }
 
 let toggleBack = document.querySelector ("#celsius-link");
 toggleBack.addEventListener ("click", celsius);
+
+
+
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+    position.coords.latitude
+  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#currentButton");
+currentLocationButton.addEventListener("click", getCurrentLocation);
